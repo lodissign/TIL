6cf1379719379ad158d498bfe25a8af269da15a1
lodissign <105443266+lodissign@users.noreply.github.com>
2022-06-14 00:29:00 +0900

    Update 0420.py

--- 0613MON/0420.py
+++ 0613MON/0420.py
@@ -4,10 +4,10 @@ import numpy as np
 
 src = cv2.imread('./data/lena.jpg', cv2.IMREAD_GRAYSCALE)
 
-minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(src)
+minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(src) # src의 최소값, 최대값, 최소값 위치, 최대값 위치를 반환. 최소/최대값이 여러 개 있는 경우 최초의 값의 위치를 반환.
 print('src:', minVal, maxVal, minLoc, maxLoc)
 
-dst = cv2.normalize(src, None, 100, 200, cv2.NORM_MINMAX)
+dst = cv2.normalize(src, None, 100, 200, cv2.NORM_MINMAX)   # 
 minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(dst)
 print('dst:', minVal, maxVal, minLoc, maxLoc)
 
